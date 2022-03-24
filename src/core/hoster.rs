0ffd22e9a4e2ab318bcdaae117fa2877b74c40b7
Jesse Laning <1132336+jamolnng@users.noreply.github.com>
2022-03-24 18:50:24 -0400

    get hosting working

--- src/core/hoster.rs
+++ src/core/hoster.rs
@@ -1,3 +1,11 @@
+use rocket::Rocket;
+
 pub fn host() -> std::result::Result<(), ()> {
-  Ok(())
+  let t = tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap().block_on(async {
+    Rocket::build().mount("/", rocket::fs::FileServer::from("output/")).ignite().await?.launch().await
+  });
+  match t {
+    Ok(_) => Ok(()),
+    Err(_) => Err(()),
+  }
 }
