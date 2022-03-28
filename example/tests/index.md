c7b3130dea2f54803c75bce55c7754f607e636c0
Jesse Laning <1132336+jamolnng@users.noreply.github.com>
2022-03-28 17:14:15 -0400

    add inline markdown tera filter

---
+++ example/tests/index.md
@@ -0,0 +1,7 @@
+---
+title: Tests
+desc: Tests
+template: _templates/default
+---
+
+[Markdown inline with HTML](/tests/inline_markdown.html)
--- src/core/builder.rs
+++ src/core/builder.rs
@@ -1,18 +1,32 @@
 use crate::core::page::{paginator, Paginator};
 use std::path::PathBuf;
-use tera::Tera;
+
+use pulldown_cmark::{html, Options, Parser};
+use tera::{to_value, Tera, Value};
+
+pub fn tera_markdown(
+  value: &Value, _: &std::collections::HashMap<String, Value>,
+) -> tera::Result<Value> {
+  let data = tera::try_get_value!("markdown", "value", String, value);
+
+  let options = Options::all();
+  let parser = Parser::new_ext(&data, options);
+  let mut html = String::with_capacity(data.len());
+  html::push_html(&mut html, parser);
+  Ok(to_value(html).unwrap())
+}
 
 pub fn build(input: &PathBuf, output: &PathBuf) -> paginator::Result<()> {
-  let mut tera = match Tera::new(
-    format!("{}/**/*.html", input.display()).as_str(),
-  ) {
-    Ok(t) => t,
-    Err(e) => {
-      println!("Parsing error(s): {}", e);
-      return Err(paginator::PaginatorError {});
-    }
-  };
+  let mut tera =
+    match Tera::new(format!("{}/**/*.html", input.display()).as_str()) {
+      Ok(t) => t,
+      Err(e) => {
+        println!("Parsing error(s): {}", e);
+        return Err(paginator::PaginatorError {});
+      }
+    };
   tera.autoescape_on(vec![]);
+  tera.register_filter("markdown", tera_markdown);
 
   let mut pages = Paginator::from(input);
   pages.render(&tera)?;
