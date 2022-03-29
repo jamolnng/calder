addc5d6b768c8f10e8e89726a3dc2b44042246c7
Jesse Laning <1132336+jamolnng@users.noreply.github.com>
2022-03-28 20:07:52 -0400

    add github pages like urls

--- src/core/hoster.rs
+++ src/core/hoster.rs
@@ -1,13 +1,24 @@
-use rocket::{catch, catchers, fs::NamedFile, Request, Rocket};
+use rocket::{catch, catchers, fs::NamedFile, routes, Request, Rocket};
 
-struct BasePath(std::path::PathBuf);
+pub struct BasePath(pub std::path::PathBuf);
 
-#[catch(404)]
-pub async fn not_found(req: &Request<'_>) -> Option<NamedFile> {
+#[catch(default)]
+pub async fn catch_default(status: rocket::http::Status, req: &Request<'_>) -> Option<NamedFile> {
   let path = req.rocket().state::<BasePath>();
   match path {
-    Some(path) => NamedFile::open(path.0.join("404.html")).await.ok(),
+    Some(path) => NamedFile::open(path.0.join(format!("{}.html", status.code))).await.ok(),
     None => None,
+  }
+}
+
+#[rocket::get("/<path..>", rank = 0)]
+pub async fn regular_file(
+  path: std::path::PathBuf, base: &rocket::State<BasePath>,
+) -> Option<NamedFile> {
+  let total_path = base.0.join(&path);
+  match total_path.extension() {
+    Some(_) => NamedFile::open(total_path).await.ok(),
+    None => NamedFile::open(total_path.with_extension("html")).await.ok(),
   }
 }
 
@@ -18,9 +29,10 @@ pub fn host(output: &std::path::PathBuf) -> std::result::Result<(), ()> {
     .unwrap()
     .block_on(async {
       Rocket::build()
-        .mount("/", rocket::fs::FileServer::from(output))
+        .mount("/", rocket::fs::FileServer::from(output).rank(-15))
+        .mount("/", routes![regular_file])
         .manage(BasePath { 0: output.clone() })
-        .register("/", catchers![not_found])
+        .register("/", catchers![catch_default])
         .ignite()
         .await?
         .launch()
