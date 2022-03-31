258ce9d80af78fff52588373c2f642ebb176c888
Jesse Laning <1132336+jamolnng@users.noreply.github.com>
2022-03-31 00:49:24 -0400

    rewrite page and paginator

--- src/core/page/page.rs
+++ src/core/page/page.rs
@@ -1,120 +1,195 @@
-use serde::{Deserialize, Serialize};
+use chrono::{NaiveDate, NaiveDateTime, Utc};
+use pulldown_cmark::{html, Options, Parser};
+use serde::{Deserialize, Deserializer, Serialize};
 use std::path::PathBuf;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(default)]
-pub struct Page {
-  pub title: String,
-  pub date: String,
-  pub desc: String,
-  pub template: String,
-  pub tags: Vec<String>,
-  pub img: String,
-  #[serde(skip_deserializing)]
-  pub build_date: String,
-  #[serde(skip_deserializing)]
-  pub path: String,
-  #[serde(skip_deserializing)]
-  pub data: String,
+#[derive(Debug)]
+pub enum PageErrorKind {
+  Tera(tera::Error),
+  Io(std::io::Error),
+  ContentType(String),
 }
 
-#[derive(Debug, Clone)]
-pub struct PageError;
+#[derive(Debug)]
+pub struct PageError {
+  #[allow(dead_code)]
+  kind: PageErrorKind,
+}
+
+impl From<tera::Error> for PageError {
+  fn from(e: tera::Error) -> Self {
+    Self { kind: PageErrorKind::Tera(e) }
+  }
+}
+
+impl From<std::io::Error> for PageError {
+  fn from(e: std::io::Error) -> Self {
+    Self { kind: PageErrorKind::Io(e) }
+  }
+}
 
 pub type Result<T> = std::result::Result<T, PageError>;
 
-impl Page {
-  pub fn has_tag(&self, tag: &str) -> bool {
-    self.tags.contains(&tag.to_string())
+#[derive(Debug, Clone, Serialize, PartialEq, PartialOrd, Eq, Ord, Hash)]
+pub struct Tag(pub String);
+
+impl Tag {
+  pub fn from(tag: String) -> Self {
+    Self { 0: Self::canonicalize(&tag) }
   }
 
-  pub fn template(&self) -> &String {
-    &self.template
+  pub fn canonicalize(tag: &str) -> String {
+    tag
+      .trim()
+      .to_lowercase()
+      .split_whitespace()
+      .collect::<Vec<&str>>()
+      .join("-")
   }
+}
 
-  pub fn set_path(&mut self, path: &PathBuf) {
-    self.path = path.to_string_lossy().replace("\\", "/");
+impl std::fmt::Display for Tag {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{}", self.0)
   }
+}
 
-  pub fn set_data(&mut self, data: String) {
-    self.data = data;
+impl<'de> Deserialize<'de> for Tag {
+  fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+  where
+    D: Deserializer<'de>,
+  {
+    Deserialize::deserialize(deserializer).map(|s| Tag::from(s))
   }
+}
 
-  pub fn render(
-    &mut self, pages: &Vec<&Self>, tera: &tera::Tera,
-  ) -> Result<()> {
-    self.render_tera(pages, tera)?;
-    Ok(())
-  }
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(default)]
+pub struct PageInfo {
+  pub title: String,
+  pub date: NaiveDate,
+  pub desc: String,
+  #[serde(deserialize_with = "from_template")]
+  pub template: String,
+  pub img: Option<String>,
+  pub tags: Vec<Tag>,
+  #[serde(skip_deserializing)]
+  pub build_date: NaiveDateTime,
+  #[serde(skip_deserializing)]
+  pub path: String,
+}
 
-  pub fn write(&self, path: &PathBuf) -> Result<()> {
-    let path = path.join(&self.path).with_extension("html");
-    print!("{}", path.display());
-    let parent = match path.parent() {
-      Some(p) => Ok(p),
-      None => Err(PageError {}),
-    }?;
-    match std::fs::create_dir_all(parent) {
-      Ok(_) => Ok(()),
-      Err(_) => Err(PageError {}),
-    }?;
-    match std::fs::write(path, &self.data) {
-      Ok(_) => {
-        println!(" ok");
-        Ok(())
-      }
-      Err(e) => {
-        println!("{e}");
-        Err(PageError {})
-      }
-    }
-  }
-
-  fn render_tera(
-    &mut self, pages: &Vec<&Self>, tera: &tera::Tera,
-  ) -> Result<()> {
-    let mut context = tera::Context::new();
-    context.insert("pages", &pages);
-    context.insert(
-      "posts",
-      &crate::core::page::Paginator::get_type_from(pages, "_templates/post"),
-    );
-    context.insert("url", &"http://127.0.0.1:8000");
-    context.insert("self", &self);
-
-    let mut template = self.template.clone();
-    if !self.template.ends_with(".html") {
-      template = format!("{template}.html");
-    }
-    if tera.get_template_names().find(|s| *s == template).is_none() {
-      template = "_templates/default.html".to_string();
-    }
-
-    let result = tera.render(&template, &context);
-    match result {
-      Ok(s) => {
-        self.data = s;
-        Ok(())
-      }
-      Err(e) => {
-        println!("{e}");
-        Err(PageError {})},
+impl Default for PageInfo {
+  fn default() -> Self {
+    Self {
+      title: String::new(),
+      date: Utc::now().naive_utc().date(),
+      desc: String::new(),
+      template: String::from("_templates/default.html"),
+      build_date: Utc::now().naive_utc(),
+      tags: Vec::new(),
+      img: None,
+      path: String::new(),
     }
   }
 }
 
-impl Default for Page {
-  fn default() -> Self {
-    Self {
-      title: String::new(),
-      date: String::new(),
-      desc: String::new(),
-      template: String::new(),
-      build_date: chrono::Utc::now().format("%Y-%m-%d").to_string(),
-      tags: Vec::new(),
-      img: String::new(),
-      path: String::new(),
-      data: String::new(),
+fn from_template<'de, D>(
+  deserializer: D,
+) -> std::result::Result<String, D::Error>
+where
+  D: Deserializer<'de>,
+{
+  let mut template: String = Deserialize::deserialize(deserializer)?;
+  if !template.ends_with(".html") {
+    template.push_str(".html");
+  }
+  Ok(template)
+}
+
+#[derive(Debug, Clone)]
+pub enum ContentType {
+  Markdown,
+  Template,
+  HTML,
+}
+
+#[derive(Debug)]
+pub struct PageContent {
+  pub kind: ContentType,
+  pub content: String,
+}
+
+#[derive(Debug)]
+pub struct Page {
+  pub info: PageInfo,
+  pub content: PageContent,
+}
+
+impl Page {
+  pub fn has_tag(&self, tag: &Tag) -> bool {
+    self.info.tags.contains(tag)
+  }
+
+  pub fn tags(&self) -> &Vec<Tag> {
+    &self.info.tags
+  }
+
+  pub fn template(&self) -> &String {
+    &self.info.template
+  }
+
+  pub fn render(
+    &mut self, tera: &tera::Tera, mut context: &mut tera::Context,
+  ) -> Result<()> {
+    match self.content.kind {
+      ContentType::Markdown => {
+        self.content = Self::render_markdown(&self.content.content);
+        self.content = self.render_template(&tera, &mut context)?;
+        Ok(())
+      }
+      ContentType::Template => {
+        self.content = self.render_template(&tera, &mut context)?;
+        Ok(())
+      }
+      ContentType::HTML => Ok(()),
     }
+  }
+
+  pub fn render_markdown(str: &String) -> PageContent {
+    let options = Options::all();
+    let parser = Parser::new_ext(&str, options);
+    let mut html = String::with_capacity(str.len() * 3 / 2);
+    html::push_html(&mut html, parser);
+    PageContent { kind: ContentType::Template, content: html }
+  }
+
+  fn render_template(
+    &self, tera: &tera::Tera, context: &mut tera::Context,
+  ) -> Result<PageContent> {
+    context.insert("self", &self.info);
+    context.insert("content", &self.content.content);
+    Ok(
+      tera
+        .render(&self.info.template, &context)
+        .map(|s| PageContent { kind: ContentType::HTML, content: s })?,
+    )
+  }
+
+  pub fn write(&self, base: &PathBuf) -> Result<()> {
+    let content = match self.content.kind {
+      ContentType::HTML => Ok(&self.content.content),
+      _ => Err(PageError {
+        kind: PageErrorKind::ContentType(format!(
+          "Content must be rendered to be written, not {:#?}",
+          self.content.kind
+        )),
+      }),
+    }?;
+    let path = base.join(&self.info.path).with_extension("html");
+    let parent = path.parent().unwrap();
+    std::fs::create_dir_all(parent)?;
+    std::fs::write(path, content)?;
+    Ok(())
   }
 }
--- src/core/page/paginator.rs
+++ src/core/page/paginator.rs
@@ -1,210 +1,325 @@
 #![allow(dead_code)]
 
 use glob::glob;
-use pulldown_cmark::{html, Options, Parser};
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 use std::path::PathBuf;
 
-use crate::core::page::Page;
+use tera::{to_value, Tera, Value};
 
-#[derive(Debug, Clone)]
-pub struct PaginatorError;
+use super::page::{ContentType, Page, PageContent, PageError, PageInfo, Tag};
+
+#[derive(Debug)]
+pub enum PaginatorErrorKind {
+  Tera(tera::Error),
+  Page(PageError),
+  Glob(glob::GlobError),
+  GlobPattern(glob::PatternError),
+  Io(std::io::Error),
+  MissingHeader(String),
+  StripPrefix(std::path::StripPrefixError),
+  YAML(serde_yaml::Error),
+}
+
+#[derive(Debug)]
+pub struct PaginatorError {
+  kind: PaginatorErrorKind,
+}
+
+impl From<tera::Error> for PaginatorError {
+  fn from(e: tera::Error) -> Self {
+    Self { kind: PaginatorErrorKind::Tera(e) }
+  }
+}
+
+impl From<PageError> for PaginatorError {
+  fn from(e: PageError) -> Self {
+    Self { kind: PaginatorErrorKind::Page(e) }
+  }
+}
+
+impl From<glob::GlobError> for PaginatorError {
+  fn from(e: glob::GlobError) -> Self {
+    Self { kind: PaginatorErrorKind::Glob(e) }
+  }
+}
+
+impl From<glob::PatternError> for PaginatorError {
+  fn from(e: glob::PatternError) -> Self {
+    Self { kind: PaginatorErrorKind::GlobPattern(e) }
+  }
+}
+
+impl From<std::io::Error> for PaginatorError {
+  fn from(e: std::io::Error) -> Self {
+    Self { kind: PaginatorErrorKind::Io(e) }
+  }
+}
+
+impl From<std::path::StripPrefixError> for PaginatorError {
+  fn from(e: std::path::StripPrefixError) -> Self {
+    Self { kind: PaginatorErrorKind::StripPrefix(e) }
+  }
+}
+
+impl From<serde_yaml::Error> for PaginatorError {
+  fn from(e: serde_yaml::Error) -> Self {
+    Self { kind: PaginatorErrorKind::YAML(e) }
+  }
+}
 
 pub type Result<T> = std::result::Result<T, PaginatorError>;
+
+#[derive(serde::Serialize)]
+pub struct Site<'a> {
+  posts: &'a Vec<PageInfo>,
+  tags: &'a BTreeSet<Tag>,
+}
 
 #[derive(Debug)]
 pub struct Paginator {
   base: PathBuf,
   pages: Vec<Page>,
-  tags: Vec<Page>,
+  tag_pages: Vec<(Tag, Page, Vec<PageInfo>)>,
+  tags: BTreeSet<Tag>,
+  tera: tera::Tera,
 }
 
 impl Paginator {
-  pub fn from(path: &PathBuf) -> Self {
-    let pages = match Self::get_pages(path) {
-      Ok(p) => p,
-      Err(_) => Vec::new(),
+  pub fn from(path: &PathBuf) -> Result<Self> {
+    let mut tera =
+      Tera::new(format!("{}/**/*.html", path.display()).as_str())?;
+    tera.autoescape_on(vec![]);
+    tera.register_filter("markdown", Self::tera_markdown);
+
+    let pages = Self::get_pages(path)?;
+    let tags = Self::get_tags(&pages);
+    let tag_pages = Self::get_tag_pages(
+      pages.iter().map(|p| p.info.clone()).collect(),
+      &tags,
+    );
+    Ok(Self { base: path.clone(), pages, tag_pages, tags, tera })
+  }
+
+  pub fn render(&mut self) -> super::page::Result<Vec<()>> {
+    let site = Site {
+      posts: &self.with_template(&String::from("_templates/post.html")),
+      tags: &self.tags,
     };
-    let mut tags = HashSet::new();
-    for page in &pages {
-      tags.extend(page.tags.iter().cloned());
-    }
-    Self { base: path.clone(), pages: pages, tags: Self::get_tags(&tags) }
+    let mut context = tera::Context::new();
+    context.insert("site", &site);
+    self
+      .pages
+      .iter_mut()
+      .map(|page| {
+        print!("Building: {}... ", page.info.path);
+        let r = page.render(&self.tera, &mut context);
+        match r {
+          Ok(_) => println!("ok"),
+          Err(_) => println!("err"),
+        }
+        r
+      })
+      .collect::<super::page::Result<Vec<()>>>()?;
+
+    self
+      .tag_pages
+      .iter_mut()
+      .map(|page| {
+        let site = Site { posts: &page.2, tags: &BTreeSet::new() };
+        let mut context = tera::Context::new();
+        context.insert("site", &site);
+        print!("Building: {}... ", page.1.info.path);
+        let r = page.1.render(&self.tera, &mut context);
+        match r {
+          Ok(_) => println!("ok"),
+          Err(_) => println!("err"),
+        }
+        r
+      })
+      .collect::<super::page::Result<Vec<()>>>()
+  }
+
+  pub fn write(&self, base: &PathBuf) -> super::page::Result<Vec<()>> {
+    self
+      .pages
+      .iter()
+      .map(|page| {
+        print!(
+          "Writing: {}... ",
+          base.join(&page.info.path).with_extension("html").display()
+        );
+        let r = page.write(base);
+        match r {
+          Ok(_) => println!("ok"),
+          Err(_) => println!("err"),
+        }
+        r
+      })
+      .collect::<super::page::Result<Vec<()>>>()?;
+
+    self
+      .tag_pages
+      .iter()
+      .map(|page| {
+        print!(
+          "Writing: {}... ",
+          base.join(&page.1.info.path).with_extension("html").display()
+        );
+        let r = page.1.write(base);
+        match r {
+          Ok(_) => println!("ok"),
+          Err(_) => println!("err"),
+        }
+        r
+      })
+      .collect()
   }
 
   pub fn pages(&self) -> &Vec<Page> {
     &self.pages
   }
 
-  pub fn with_tag(&self, tag: &str) -> Vec<&Page> {
-    Self::with_tag_from(&self.pages, tag)
+  pub fn with_tag(&self, tag: &Tag) -> Vec<&PageInfo> {
+    self.pages.iter().filter(|p| p.has_tag(tag)).map(|p| &p.info).collect()
   }
 
-  pub fn with_tag_from<'a>(pages: &'a Vec<Page>, tag: &str) -> Vec<&'a Page> {
-    let mut r = Vec::new();
-    for page in pages {
-      if page.has_tag(tag) {
-        r.push(page);
-      }
-    }
-    r
+  pub fn with_template(&self, template: &String) -> Vec<PageInfo> {
+    self
+      .pages
+      .iter()
+      .filter(|p| p.template() == template)
+      .map(|p| &p.info)
+      .cloned()
+      .collect()
   }
 
-  pub fn get_type(&self, t: &str) -> Vec<&Page> {
-    Self::get_type_from(&self.pages.iter().collect(), t)
+  fn get_tags(pages: &Vec<Page>) -> BTreeSet<Tag> {
+    pages.iter().flat_map(|p| p.tags().clone()).collect()
   }
 
-  pub fn get_type_from<'a>(
-    pages: &Vec<&'a Page>, template: &str,
-  ) -> Vec<&'a Page> {
-    let mut r = Vec::new();
-    for page in pages {
-      if page.template() == template {
-        r.push(*page);
-      }
-    }
-    r
-  }
-
-  pub fn render(&mut self, tera: &tera::Tera) -> Result<()> {
-    let pages = self.pages.clone();
-    let p = pages.iter().collect::<Vec<&Page>>();
-    for page in &mut self.pages {
-      match page.render(&p, tera) {
-        Ok(_) => {}
-        Err(_) => {}
-      }
-    }
-    for tag in &mut self.tags {
-      let p = pages
-        .iter()
-        .filter(|page| page.has_tag(tag.tags.first().unwrap()))
-        .collect();
-      match tag.render(&p, tera) {
-        Ok(_) => {}
-        Err(_) => {}
-      }
-    }
-    Ok(())
-  }
-
-  pub fn write(&self, path: &PathBuf) -> Result<()> {
-    for page in &self.pages {
-      match page.write(path) {
-        Ok(_) => {}
-        Err(_) => {}
-      }
-    }
-    for page in &self.tags {
-      match page.write(path) {
-        Ok(_) => {}
-        Err(_) => {}
-      }
-    }
-    Ok(())
-  }
-
-  fn get_tags(tags: &HashSet<String>) -> Vec<Page> {
-    let mut r = Vec::new();
-    for tag in tags {
-      let s = tag.to_lowercase();
-      r.push(Page {
-        title: s.clone(),
-        desc: s.clone(),
-        template: "_templates/tag.html".to_string(),
-        tags: vec![s.clone()],
-        path: format!("tags/{s}"),
-        ..Default::default()
+  fn get_tag_pages<'b>(
+    pages: Vec<PageInfo>, tags: &BTreeSet<Tag>,
+  ) -> Vec<(Tag, Page, Vec<PageInfo>)> {
+    tags
+      .iter()
+      .map(|tag| {
+        let info = super::page::PageInfo {
+          title: tag.0.clone(),
+          desc: tag.0.clone(),
+          template: String::from("_templates/tag.html"),
+          path: format!("tags/{tag}.html"),
+          ..Default::default()
+        };
+        let content = super::page::PageContent {
+          kind: super::page::ContentType::Template,
+          content: String::new(),
+        };
+        let ps =
+          pages.iter().filter(|p| p.tags.contains(tag)).cloned().collect();
+        (tag.clone(), Page { info, content }, ps)
       })
-    }
-    r
+      .collect()
   }
 
-  fn get_pages(path: &PathBuf) -> Result<Vec<Page>> {
+  fn get_pages(base: &PathBuf) -> Result<Vec<Page>> {
     let mut r = Vec::new();
-    if let Some(files) = Self::get_markdown_files(path) {
-      for file in files {
-        if let Ok(file) = file {
-          if file.is_file() {
-            if let Ok(lines) = Self::read_lines(&file) {
-              let mut first = false;
-              let mut second = false;
-              let mut page_str = String::new();
-              let mut data = String::new();
-              for line in lines {
-                if let Ok(line) = line {
-                  if first && second {
-                    data.push_str(&format!("{line}\n"));
-                  } else if line.trim() == "---" {
-                    if !first {
-                      first = true;
-                    } else {
-                      second = true;
-                    }
-                  } else if !line.trim().is_empty() && !first {
-                    break; // TODO: error
-                  } else {
-                    page_str.push_str(&format!("{line}\n"));
-                  }
-                }
-              }
-              let file = PathBuf::from(file.strip_prefix(path).unwrap());
-              if first && second {
-                let page = serde_yaml::from_str::<Page>(&page_str);
-                if let Ok(mut page) = page {
-                  page.set_path(&file.with_extension("html"));
-                  page.set_data(Self::render_markdown(&data));
-                  r.push(page);
-                }
-              } else if !first && !second {
-                let title = Self::pretty_unknown_title(&file);
-                r.push(Page {
-                  title: title,
-                  path: file.to_string_lossy().replace("\\", "/"),
-                  data: data,
-                  ..Page::default()
-                })
-              }
-            }
-          }
-        }
+    for file in Self::get_markdown_files(base)? {
+      let file = file?;
+      if file.is_file() {
+        r.push(Self::process_md_file(&base, &file)?);
       }
     }
-    for file in Self::get_html_files(path) {
-      if let Ok(file) = file {
-        if file.is_file() {
-          if let Ok(s) = std::fs::read_to_string(&file) {
-            let file = PathBuf::from(file.strip_prefix(path).unwrap());
-            let title = Self::pretty_unknown_title(&file);
-            r.push(Page {
-              title: title,
-              path: file.to_string_lossy().replace("\\", "/"),
-              data: s,
-              template: file.to_string_lossy().replace("\\", "/"),
-              ..Page::default()
-            })
-          }
-        }
+    let files = Self::get_html_files(base)?;
+    r.reserve(files.len());
+    for file in files {
+      let file = file?;
+      if file.is_file() {
+        r.push(Self::process_html_file(&base, &file)?);
       }
     }
     Ok(r)
   }
 
-  fn render_markdown(data: &String) -> String {
-    let options = Options::all();
-    let parser = Parser::new_ext(&data, options);
-    let mut html = String::with_capacity(data.len());
-    html::push_html(&mut html, parser);
-    html
+  fn process_md_file(base: &PathBuf, file: &PathBuf) -> Result<Page> {
+    let infile = std::fs::File::open(file)?;
+    let reader = std::io::BufReader::new(&infile);
+    let lines = Self::full_lines(reader);
+    let mut first = false;
+    let mut second = false;
+    let mut header = String::new();
+    let mut content = String::with_capacity(infile.metadata()?.len() as usize);
+    for line in lines {
+      let line = line?;
+      if first && second {
+        content.push_str(&line);
+      } else {
+        let lt = line.trim();
+        if lt.starts_with("---") {
+          if first {
+            second = true;
+          } else {
+            first = true;
+          }
+        } else if !lt.is_empty() && !first {
+          return Err(PaginatorError {
+            kind: PaginatorErrorKind::MissingHeader(format!(
+              "Missing YAML header from file: {}",
+              file.display()
+            )),
+          });
+        } else {
+          header.push_str(&line);
+        }
+      }
+    }
+    let path = file.strip_prefix(base)?.with_extension("html");
+    if first && second {
+      let mut info = serde_yaml::from_str::<PageInfo>(&header)?;
+      info.path = path.to_string_lossy().replace("\\", "/");
+      let content = PageContent { kind: ContentType::Markdown, content };
+      return Ok(Page { info, content });
+    } else if !first && !second {
+      let title = Self::pretty_unknown_title(&path);
+      let info = PageInfo {
+        title,
+        path: path.to_string_lossy().replace("\\", "/"),
+        ..Default::default()
+      };
+      let content = PageContent { kind: ContentType::Markdown, content };
+      return Ok(Page { info, content });
+    } else {
+      return Err(PaginatorError {
+        kind: PaginatorErrorKind::MissingHeader(format!(
+          "Missing YAML header from file: {}",
+          file.display()
+        )),
+      });
+    }
   }
 
-  fn pretty_unknown_title(file: &PathBuf) -> String {
+  fn process_html_file(base: &PathBuf, file: &PathBuf) -> Result<Page> {
+    let s = std::fs::read_to_string(&file)?;
+    let file = file.strip_prefix(base)?.to_string_lossy().replace("\\", "/");
+    Ok(Page {
+      info: PageInfo {
+        title: Self::pretty_unknown_title(&file),
+        template: file.clone(),
+        path: file,
+        ..Default::default()
+      },
+      content: PageContent { kind: ContentType::Template, content: s },
+    })
+  }
+
+  fn pretty_unknown_title<P: AsRef<std::path::Path>>(file: P) -> String {
     let key = file
+      .as_ref()
       .file_name()
       .unwrap()
       .to_string_lossy()
       .to_string()
-      .replace(".md", "");
+      .replace(".md", "")
+      .replace(".html", "");
     let mut v = key
       .replace('_', " ")
       .split(' ')
@@ -220,15 +335,29 @@ impl Paginator {
     v.join(" ")
   }
 
-  fn get_html_files(path: &PathBuf) -> Vec<glob::GlobResult> {
-    glob(format!("{}/[!_]*/**/*.html", path.display()).as_str())
-      .unwrap()
-      .chain(glob(format!("{}/*.html", path.display()).as_str()).unwrap())
-      .collect()
+  fn get_html_files(path: &PathBuf) -> Result<Vec<glob::GlobResult>> {
+    let g1 = glob(format!("{}/[!_]*/**/*.html", path.display()).as_str())?;
+    let g2 = glob(format!("{}/*.html", path.display()).as_str())?;
+    Ok(g1.chain(g2).collect())
   }
 
-  fn get_markdown_files(path: &PathBuf) -> Option<glob::Paths> {
-    glob(format!("{}/**/*.md", path.display()).as_str()).ok()
+  fn get_markdown_files(
+    path: &PathBuf,
+  ) -> std::result::Result<glob::Paths, glob::PatternError> {
+    glob(format!("{}/**/*.md", path.display()).as_str())
+  }
+
+  fn full_lines(
+    mut input: impl std::io::BufRead,
+  ) -> impl Iterator<Item = std::io::Result<String>> {
+    std::iter::from_fn(move || {
+      let mut vec = String::new();
+      match input.read_line(&mut vec) {
+        Ok(0) => None,
+        Ok(_) => Some(Ok(vec)),
+        Err(e) => Some(Err(e)),
+      }
+    })
   }
 
   fn read_lines(
@@ -237,5 +366,12 @@ impl Paginator {
 where {
     let file = std::fs::File::open(filename)?;
     Ok(std::io::BufRead::lines(std::io::BufReader::new(file)))
+  }
+
+  fn tera_markdown(
+    value: &Value, _: &std::collections::HashMap<String, Value>,
+  ) -> tera::Result<Value> {
+    let data = tera::try_get_value!("markdown", "value", String, value);
+    Ok(to_value(Page::render_markdown(&data).content).unwrap())
   }
 }
